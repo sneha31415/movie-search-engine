207a7d513540314c0f1f8cf097ef50d411a90914
sneha31415 <sneha.singh.31415@gmail.com>
2026-02-08 13:15:01 +0530

    initiated the react app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Text toDisplay={"hello sneha"}/>
+      <Text toDisplay={"whatsup"}> </Text>
+    </>
+  )
+}
+
+
+// components in react
+function Text({toDisplay}) {
+  return (
+     <div>
+    <p>{toDisplay}</p>
+  </div>
+  )
+ 
+}
+export default App
