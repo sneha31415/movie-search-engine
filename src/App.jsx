4f0bf4bee0b30e583a51d64b3cff4f9351279a95
sneha31415 <sneha.singh.31415@gmail.com>
2026-02-11 22:11:39 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const movieNumber = 1;
   return (
     <>
-      <Text toDisplay={"hello sneha"}/>
-      <Text toDisplay={"whatsup"}> </Text>
+      {/* first set of braces denote we are putting a variable, 
+    next set of braces denote we are putting an object  */}
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "3 idiots", release_date: "2017" }}></MovieCard>
+      ) : (
+        <MovieCard movie={{ title: "3 idiots", release_date: "2017" }}></MovieCard>
+      )}
+
+      {/* way 2 */}
+      {movieNumber === 1 && <MovieCard movie={{ title: "3 idiots", release_date: "2017" }}></MovieCard>}
     </>
-  )
+  );
 }
 
 
-// components in react
-function Text({toDisplay}) {
-  return (
-     <div>
-    <p>{toDisplay}</p>
-  </div>
-  )
- 
-}
 export default App
