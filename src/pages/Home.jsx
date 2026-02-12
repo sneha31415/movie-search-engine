7c1bbbf2b57245a9cd174896ddff098e0cb8b5ec
sneha31415 <sneha.singh.31415@gmail.com>
2026-02-12 11:01:04 +0530

    added search button in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,17 @@ function Home() {
         { id: 2, title: "I", release_date: "2019" },
         { id: 3, title: "badshaah", release_date: "1999" }
     ]
+
+    const handleSearch = () => {
+
+    }
+
     return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search for movies..." className="search-input"
+            
+            />
+        </form>
         <div className="mvoies-grid">
             {/* what comes after the arrow is the component we want to return for every instance of a movie
             Whenever you dynamically render we need key */}
